4bbd3eb8390dd0b2122697f738e15cf483312344
Ivo Nenchev <ivo.nenchev@portalsystems.de>
2023-01-06 15:31:39 +0200

    adding comments, testing

--- SitePoolDefaults/Custom/aoWebApp_TimerJob_AddCompanies.js
+++ SitePoolDefaults/Custom/aoWebApp_TimerJob_AddCompanies.js
@@ -3,16 +3,16 @@
 
 const configuration = {
 	listUrl: '/Lists/ResultList',
-	masterBafUrl: '/pscBAF/TimerJobsConfiguration',
+	masterBafTimerJobConfigUrl: '/pscBAF/TimerJobsConfiguration',
 	lookUpFields: {
-		field1: 'DefaultLookUp1/Title',
-		field2: 'DefaultLookUp2/Title',
+		lookUpFieldName1: 'DefaultLookUp1/Title',
+		lookUpFieldName2: 'DefaultLookUp2/Title',
 	},
-	resultField: 'Result',
+	resultFieldName: 'Result',
 };
 
 /**
- *
+ * Function
  * @param {import('ShareflexRules').default} _rules
  */
 export async function addCompanies(_rules) {
@@ -20,15 +20,19 @@ export async function addCompanies(_rules) {
 	const webUrl = rules.Context.siteUrl;
 	const listUrl = rules.Utils.combineUrls(webUrl, configuration.listUrl);
 
+	//Setting the batch operations
 	const batchOperations = rules.createBatchOperations(webUrl);
+	//Amount of operations per single batched request
 	const batchCount = 60;
-	const processedItemsLimit = 2500;
+	//Amount of items per single get request
+	const processedItemsLimit = 10;
 
 	const resultItems = [];
+	//Getting items that are filtered by empty result field
 	await rules.processItems({
 		listUrl,
-		fields: `ID,${configuration.resultField},${configuration.lookUpFields.field1},${configuration.lookUpFields.field2}}`,
-		filter: `${configuration.resultField} eq null`,
+		fields: `ID,${configuration.resultFieldName},${configuration.lookUpFields.lookUpFieldName1},${configuration.lookUpFields.lookUpFieldName2}`,
+		filter: `${configuration.resultFieldName} eq null`,
 		processedItemsLimit,
 		callback: ({ items }) => {
 			resultItems.push(...items);
@@ -36,13 +40,15 @@ export async function addCompanies(_rules) {
 		},
 	});
 
+	//Filtering the result to get all items that have value in Company1 or Company2 field
 	const emptyResultItems = resultItems.filter((item) => item.DefaultLookUp1.length != 0 || item.DefaultLookUp2.length != 0);
 
+	//Disabling the timer job configuration if there is no items to be updated
 	if (!emptyResultItems.length) {
 		const timerJobConfigItem = await getTimerJobItem(rules, webUrl);
 
 		await rules.updateItem({
-			listUrl: rules.Utils.combineUrls(webUrl, configuration.masterBafUrl),
+			listUrl: rules.Utils.combineUrls(webUrl, configuration.masterBafTimerJobConfigUrl),
 			itemId: timerJobConfigItem[0].ID,
 			fields: rules.createItemFields().setChoice('TimerJobEnabled', 'No'),
 			eventsActivated: true,
@@ -51,6 +57,7 @@ export async function addCompanies(_rules) {
 		return;
 	}
 
+	//Updating items to add the company names into the empty result field
 	for (const emptyResultItem of emptyResultItems) {
 		let finalResultValue = '';
 
@@ -65,7 +72,7 @@ export async function addCompanies(_rules) {
 		batchOperations.addUpdateItem({
 			listUrl,
 			itemId: emptyResultItem.ID,
-			fields: rules.createItemFields().setText('Result', finalResultValue),
+			fields: rules.createItemFields().setText(configuration.resultFieldName, finalResultValue),
 		});
 	}
 
@@ -74,9 +81,15 @@ export async function addCompanies(_rules) {
 	return result;
 }
 
+/**
+ * Function that returns the the timer job configuration file to be disabled after it's finished
+ * @param {Object} rules
+ * @param {string} webUrl
+ * @returns {Promise<Object>}
+ */
 async function getTimerJobItem(rules, webUrl) {
 	const timerJobItems = await rules.getItems({
-		listUrl: rules.Utils.combineUrls(webUrl, configuration.masterBafUrl),
+		listUrl: rules.Utils.combineUrls(webUrl, configuration.masterBafTimerJobConfigUrl),
 		fields: 'ID,LinkFilename',
 	});
 	const [foundItem] = timerJobItems.filter((item) => item.LinkFilename === 'AddCompanies_TimerJob_Configuration.js');
