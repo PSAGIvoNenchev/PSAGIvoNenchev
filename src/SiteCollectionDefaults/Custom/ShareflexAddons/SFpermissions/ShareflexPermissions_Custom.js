7f058041a5dab744d7aedf83a09a7c8a0020c7ae
Ivo Nenchev <ivo.nenchev@portalsystems.de>
2023-01-10 12:46:37 +0200

    adding comments

--- src/SiteCollectionDefaults/Custom/ShareflexAddons/SFpermissions/ShareflexPermissions_Custom.js
+++ src/SiteCollectionDefaults/Custom/ShareflexAddons/SFpermissions/ShareflexPermissions_Custom.js
@@ -21,14 +21,17 @@ export async function customFnForOssenberg(options) {
 
 	rules.logInfo('test start');
 
+	//getting the ID's of the required roles
 	let contributeRoleDefId = roleDefObj.getRoleDefId('Contribute');
 	let readRoleDefId = roleDefObj.getRoleDefId('Read');
 
+	//getting the existing role names that will be used to find the matches between them and the client/department field values
 	const getExistingRoleNames = await rules.getItems({
 		listUrl: rules.Utils.combineUrls(rules.Context.siteUrl, configuration.peRolesUrl),
 		fields: 'ID,Title',
 	});
 
+	//getting the newly created/saved item to get the values of the client/department fields
 	const getAddedItem = await rules.getItemById({
 		listUrl,
 		itemId,
@@ -38,10 +41,12 @@ export async function customFnForOssenberg(options) {
 	const clientNames = [];
 	const departmentNames = [];
 
+	//getting the first part of the clients name/title 'Erwin Kowsky GmbH & Co. KG' => 'Erwin'
 	for (const clientName of getAddedItem.Clients) {
 		const firstPartOfClientName = clientName.Title.split(' ');
 		clientNames.push(firstPartOfClientName[0]);
 	}
+	//getting the department names only
 	for (const departmentName of getAddedItem.Departments) {
 		departmentNames.push(departmentName.Title);
 	}
@@ -51,12 +56,14 @@ export async function customFnForOssenberg(options) {
 	const contributeMembers = [];
 	const readMembers = [];
 
+	//getting only these Roles that are matched by the condition bellow
 	for (const clientName of clientNames) {
 		for (const departmentName of departmentNames) {
 			getAddedRoleNames.push(...getExistingRoleNames.filter((roleName) => roleName.Title.startsWith(clientName) && roleName.Title.includes(departmentName)));
 		}
 	}
 
+	//sorting which Role must have Contribute or Read rights
 	for (const roleName of getAddedRoleNames) {
 		if (roleName.Title.endsWith('-L')) {
 			readMembers.push(roleName.Title);
@@ -66,6 +73,8 @@ export async function customFnForOssenberg(options) {
 	}
 
 	await rules.breakItemRoleInheritance({ listUrl, itemId, clear: true });
+
+	//creating the virtual permission set that will be used to assign the correct item permissions
 	let permissionMappings = rules.createPermissionMappings();
 
 	permissionMappings.addRoleDefId(contributeRoleDefId, contributeMembers);
