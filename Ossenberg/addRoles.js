2186b501d76ad5b36606e3e915a344964e2b5fc7
Ivo Nenchev <ivo.nenchev@portalsystems.de>
2023-01-11 12:10:46 +0200

    adding dummy file to get an item

--- Ossenberg/addRoles.js
+++ Ossenberg/addRoles.js
@@ -10,17 +10,70 @@ import { config } from 'aoWebApp_Defaults_CUSTOM';
 
 const rules = new ShareflexRules(config);
 
-async function createItems() {
-	const listUrl = rules.Utils.combineUrls(rules.Context.siteUrl, '/Administration/Lists/ResultList');
+/**
+ * Function init and provide keys for test cases
+ * @param {import('ShareflexRules').TestCaseOptions} options
+ */
+export function initTestCaseProperties(options) {
+	const { rules, helpers, name } = options;
+	const moduleName = options.customOptions.moduleName;
 
-	const createItem = await rules.createItem({
+	const customOptions = options.customOptions;
+	const { addons, flow, notifications, permissions } = customOptions;
+
+	/** @type {import('ShareflexRules').TestCaseResult} */
+	const testResult = {
+		result: false,
+		failedMessage: '',
+	};
+	const testCaseId = rules.Utils.generateGUID({ format: '####-####' });
+
+	return {
+		rules,
+		addons,
+		flow,
+		permissions,
+		notifications,
+		helpers,
+		moduleName,
+		testCaseId,
+		testCaseName: name,
+		testResult,
+	};
+}
+
+/**
+ * Test to create context list item and folders
+ * @param {import('ShareflexRules').TestCaseOptions} options
+ */
+async function createItems(options) {
+	const { permissions, roleDefObj } = initTestCaseProperties(options);
+
+	const listUrl = rules.Utils.combineUrls(rules.Context.siteUrl, '/Workspace/DocumentsNew');
+	//getting the ID's of the required roles
+	let contributeRoleDefId = roleDefObj.getRoleDefId('Contribute');
+	let readRoleDefId = roleDefObj.getRoleDefId('Read');
+
+	const item = await rules.getItemById({
 		listUrl,
-		fields: rules.createItemFields().setText('Title', 'test1'),
-		eventsActivated: true,
+		itemId: 3,
+		fields: 'qmClient,qmDepartment',
 	});
 	debugger;
 
-	return createItem;
+	let permissionMappings = rules.createPermissionMappings();
+
+	permissionMappings.addRoleDefId(contributeRoleDefId, []);
+	permissionMappings.addRoleDefId(readRoleDefId, []);
+
+	const assignPermissions = await rules.assignItemPermissions({
+		listUrl,
+		itemId: 3,
+		permissionMappings,
+	});
+	debugger;
+	return assignPermissions;
 }
 
+// @ts-ignore
 await createItems();
